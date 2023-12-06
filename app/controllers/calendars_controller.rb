2d65d2c432e64beefab1d847e9245f8f2e9f3de5
eiriinaba <ushinohanako613@gmail.com>
2023-12-06 20:08:52 +0900

    Issue1再々修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,17 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-<<<<<<< Updated upstream
       days = {month: (@todays_date + x).month, date: (@todays_date+x).day, :plans => today_plans}
-=======
-
-      wday_num = (@todays_date + x).wday
-      if wday_num >= 7
-        wday_num = wday_num -7
-      end
-
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, :wday => wdays[wday_num]}
->>>>>>> Stashed changes
       @week_days.push(days)
     end
 
